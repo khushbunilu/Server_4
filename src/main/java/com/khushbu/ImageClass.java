b505c9bb70c9a72f2a564e556e1a1b366343f90d
khushbunilu <khushbunilu@gmail.com>
2016-05-31 17:00:07 +0530

    made image api

--- src/main/java/com/khushbu/ImageClass.java
+++ src/main/java/com/khushbu/ImageClass.java
@@ -1,25 +1,52 @@
 package com.khushbu;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Base64;
 
 /**
  * Created by khushbu on 31/5/16.
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @Path("imageclass")
 
 public class ImageClass {
 
-    @GET
+    @POST
     @Path("getimage")
     @Produces(MediaType.TEXT_PLAIN)
-    public String GetImage(String s)
+    public String GetImage(@FormParam("Image")String s)
     {
-       return ("hello");
+        String imageDataBytes = s.substring(s.indexOf(",")+1);
+
+        System.out.println("img is "+s);
+       // InputStream stream = new ByteArrayInputStream(Base64.decode(imageDataBytes.getBytes(), Base64.DEFAULT));
+
+        byte[] decoded = Base64.getDecoder().decode(s);
+        System.out.println(decoded);
+        return ("img is "+s+"\n\n decoded is "+decoded);
     }
+
+
+
 
 
     @GET
